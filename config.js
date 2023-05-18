4517d7b5ee0505e73d80fc544afe06b058328af2
ola265 <68593577+ola265@users.noreply.github.com>
2023-05-18 18:24:26 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,8 +9,8 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? 'default' : process.env.ALIVE_MSG,
 LANG: process.env.LANG === undefined ? 'EN' : process.env.LANG,
-ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://i.imgur.com/v8iuP8v.jpg' : process.env.ALIVE_LOGO,
-FOOTER: process.env.FOOTER === undefined ? 'ᴀꜱᴛʀᴏ-ᴍᴅ ᴘᴜʙʟɪᴄ ꜱᴛᴀʙʟᴇ' : process.env.FOOTER,
+ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://i.imgur.com/aSm8fGZ.jpeg' : process.env.ALIVE_LOGO,
+FOOTER: process.env.FOOTER === undefined ? 'VIRUS-BOT PUBLIC STABLE' : process.env.FOOTER,
 CAPTION: process.env.CAPTION === undefined ? '*ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ ᴀꜱᴛʀᴏ-ᴍᴅ*' : process.env.CAPTION,
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*Inbox not allowed' : process.env.INBOX_BL_MSG,
